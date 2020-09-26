d591cc24434e446573d3f91211482df844fe44ec
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-09-26 17:15:24 +0200

    Debug mode off

--- mapaly/settings.py
+++ mapaly/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_l-+!5#^@2w4@ry(u%=2(x=3nlu421xffokt%plqnz*cu6qu(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "mapaquiz.com",
